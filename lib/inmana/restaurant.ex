152053da50937b7af6e8bc1cf7b3eebb319b6566
Pedro Neto <pedromneto97@gmail.com>
2021-04-22 20:10:14 -0300

    feat: cria model de restaurante

---
+++ lib/inmana/restaurant.ex
@@ -0,0 +1,29 @@
+defmodule Inmana.Restaurant do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:email, :name]
+
+  @derive {Jason.Encoder, only: [:id] ++ @required_params}
+
+  schema "restaurants" do
+    field :email, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:name, min: 2)
+    |> validate_format(
+      :email,
+      ~r/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
+    )
+    |> unique_constraint([:email])
+  end
+end
