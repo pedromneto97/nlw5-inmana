8883ed54b595c9b899a28954398ec4f57c31ccea
Pedro Neto <pedromneto97@gmail.com>
2021-04-24 21:00:04 -0300

    feat: Manda os e-mails de forma assíncrona

--- lib/inmana/supplies/expiration_notification.ex
+++ lib/inmana/supplies/expiration_notification.ex
@@ -5,8 +5,13 @@ defmodule Inmana.Supplies.ExpirationNotification do
   def send do
     data = GetByExpiration.call()
 
-    Enum.each(data, fn {to_email, supplies} ->
-      to_email |> ExpirationEmail.create(supplies) |> Mailer.deliver_now!()
+    Task.async_stream(data, fn {to_email, supplies} ->
+      send_email(to_email, supplies)
     end)
+    |> Stream.run()
+  end
+
+  defp send_email(to_email, supplies) do
+    to_email |> ExpirationEmail.create(supplies) |> Mailer.deliver_now!()
   end
 end
